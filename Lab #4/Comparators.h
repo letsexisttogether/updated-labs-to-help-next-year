c799e9932cf9860d5c9685b50059aad896bae748
letsexisttogether <bleck2star@gmail.com>
2022-10-23 15:30:04 +0300

    Added another comporator

--- Lab #4/Comparators.h
+++ Lab #4/Comparators.h
@@ -3,12 +3,22 @@
 #define _COMP_BEGIN_ namespace Comps {
 #define _COMP_END_					 }
 
+#include <stdint.h>
+
+#include "Int32Holder.h"
+
 _COMP_BEGIN_
 
 template <class _CType>
-bool SimpleComp(_CType leftValue, _CType rightValue)
+bool SimpleComp(const _CType& leftValue, const _CType& rightValue)
 {
 	return leftValue == rightValue;
+}
+
+template <class _FType, class _SType>
+bool DiffTypeValuesComp(const _FType& first, const _SType second)
+{
+	return first == second;
 }
 
 _COMP_END_
