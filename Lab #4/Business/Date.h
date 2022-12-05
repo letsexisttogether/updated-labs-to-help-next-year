5193a4b309f6874b1e4232d692e19a6e3c9aae03
letsexisttogether <bleck2star@gmail.com>
2022-12-05 21:53:14 +0200

    Divided (where it was possible) business files to .h and .cpp files. Translated the files to the folder

---
+++ Lab #4/Business/Date.h
@@ -0,0 +1,37 @@
+#ifndef DATE
+#define DATE
+
+#include <iostream>
+
+struct Date
+{
+private:
+	friend class DateValidator;
+
+private:
+	uint16_t m_Day;
+	uint16_t m_Month;
+	uint16_t m_Year;
+
+public:
+	Date() = delete;
+	Date(const Date& date) = default;
+	Date(Date&& date) = default;
+
+	Date(uint16_t day, uint16_t month, uint16_t year);
+
+	~Date() = default;
+
+	uint16_t day() const noexcept;
+	uint16_t month() const noexcept;
+	uint16_t year() const noexcept;
+
+	Date& operator = (const Date&) = default;
+	Date& operator = (Date&&) = default;
+
+	bool operator == (const Date& extraObj) const noexcept;
+
+	friend std::ostream& operator << (std::ostream& writeStream, const Date& thisObj) noexcept;
+};
+
+#endif // !DATE
